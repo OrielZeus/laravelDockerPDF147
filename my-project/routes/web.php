dcca11124416b2f7d11a99fb1a1fb23662764954
elorihuela <elorihuela@github.com>
2022-12-20 13:48:30 -0400

    After creating a Laravel repository, the PDF./ library was added, in which the welcome blade page was updated to obtain the image

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use BigFish\PDF417\PDF417;
+use BigFish\PDF417\Renderers\ImageRenderer;
+use BigFish\PDF417\Renderers\SvgRenderer;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +18,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('welcome');
-});
+    $pdf417 = new PDF417();
+    $data = $pdf417->encode('www.facebook.com/oriel401');
+
+    $renderer = new ImageRenderer([
+        'format' => 'data-url'
+    ]);
+    $img = $renderer->render($data);
+        return view('welcome', compact('img'));
+    });
